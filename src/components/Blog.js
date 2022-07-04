1f5eb51f66123f8c52059e65e7e46c37e74dfdc7
Manu <manugeo13@gmail.com>
2022-07-04 21:09:52 +0530

    Blog - Added expanded view

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,28 @@
-const Blog = ({blog}) => (
-  <div style={{ marginTop: '20px'}}>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const { title, author, url, likes, user } = blog || {};
 
-export default Blog
+  return (
+    <div style={blogStyle}>
+      {title} {author}
+      <button onClick={() => setIsExpanded(!isExpanded)}>{isExpanded ? 'hide' : 'view'}</button>
+      {isExpanded && (
+        <>
+          <p>{url}</p>
+          <p>likes {likes} <button onClick={() => { }}>like</button></p>
+          <p>{user.name}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
